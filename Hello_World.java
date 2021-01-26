178d6b266bcd880f7a1854eb9fb5b4f972e31a53
FilipKlominek <71869121+FilipKlominek@users.noreply.github.com>
2021-01-26 12:39:27 +0100

    Add files via upload

---
+++ Hello_World.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Hello_World {
+
+    public static void main(String[] args) {
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter("output.txt"));
+
+            for (int i = 0; i < 2; i++) {
+                bw.write("Hello, world!");
+                bw.newLine();
+            }
+
+            bw.close();
+        } catch (IOException e) {
+            System.out.println("Unable to write to file...");
+        }
+    }
+}
